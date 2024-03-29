12f14464de8ba16ae48a8cf9bc880bd5a0c94886
DhebaRam <dhebaram1999@gmail.com>
2024-03-29 18:11:24 +0530

    google event add

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'dart:async';
 import 'dart:developer' as log_print;
+import 'dart:developer';
 import 'dart:math' show cos, sqrt, asin;
 
 import 'package:firebase_core/firebase_core.dart';
@@ -13,6 +14,13 @@ import 'package:google_map_demo/users_model.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:url_launcher/link.dart';
 import 'firestore_service.dart';
+
+//google event
+import 'package:google_sign_in/google_sign_in.dart';
+import 'package:googleapis/calendar/v3.dart' as GoogleAPI;
+import 'package:http/io_client.dart' show IOClient, IOStreamedResponse;
+import 'package:http/http.dart' show BaseRequest, Response;
+import 'package:syncfusion_flutter_calendar/calendar.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -49,7 +57,8 @@ class MyApp extends StatelessWidget {
         useMaterial3: true,
       ),
       // home: MapPage(),
-      home: const MapScreen(),
+      // home: const MapScreen(),
+      home: const MyHomePage(),
     );
   }
 }
@@ -231,6 +240,236 @@ class _MapPageState extends State<MapPage> {
   }
 }*/
 
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({super.key});
+
+  @override
+  State<MyHomePage> createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  late CalendarController _controller;
+
+  final GoogleSignIn _googleSignIn = GoogleSignIn(
+    // clientId: '[501041666177-j3eunanijg6a52jvk5qmjfc44r89at9m.apps.googleusercontent.com]',
+    scopes: <String>[GoogleAPI.CalendarApi.calendarScope],
+    forceCodeForRefreshToken: true,
+  );
+
+  GoogleSignInAccount? _currentUser;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = CalendarController();
+    _googleSignIn.onCurrentUserChanged.listen((GoogleSignInAccount? account) {
+      setState(() {
+        _currentUser = account;
+      });
+      if (_currentUser != null) {
+        // getGoogleEventsData();
+      }
+    });
+    _googleSignIn.signInSilently();
+  }
+
+  Stream<List<GoogleAPI.Event>> getGoogleEventsData() async*{
+    final List<GoogleAPI.Event> appointments = <GoogleAPI.Event>[];
+    try {
+      final GoogleSignInAccount? googleUser = await _googleSignIn.signIn();
+      log('googleUser ---> ${googleUser?.email}');
+      final GoogleAPIClient httpClient = GoogleAPIClient(await googleUser!.authHeaders);
+
+      final GoogleAPI.CalendarApi calendarApi = GoogleAPI.CalendarApi(httpClient);
+      final GoogleAPI.Events calEvents = await calendarApi.events.list(
+        "primary",
+      );
+
+      if (calEvents.items != null) {
+        for (int i = 0; i < calEvents.items!.length; i++) {
+          final GoogleAPI.Event event = calEvents.items![i];
+          if (event.start == null) {
+            continue;
+          }
+          log('Cal Event ---> ${event.description} .. ${event.status}');
+          appointments.add(event);
+        }
+      }
+      yield appointments;
+    } catch (e) {
+      log('Error --- > ${e.toString()}');
+      yield appointments;
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('Event Calendar'),
+      ),
+      body: StreamBuilder(
+        stream: getGoogleEventsData(),
+        builder: (BuildContext context, AsyncSnapshot snapshot) {
+          return Stack(
+            children: [
+              SfCalendar(
+                onTap: (CalendarTapDetails details) async {
+                  // details.appointments!.map((e) => log('On Tap --> ${e.first}')).toList();
+
+                  for (int i = 0; i < details.appointments!.length; i++) {
+                    final GoogleAPI.Event event = details.appointments![i];
+                    log('Cal Event ---> ${event.description} .. ${event.status}');
+
+                      final GoogleAPIClient httpClient = GoogleAPIClient(await _currentUser!.authHeaders);
+                      final GoogleAPI.CalendarApi calendarApi = GoogleAPI.CalendarApi(httpClient);
+                    await calendarApi.events.delete('primary', event.id.toString());
+                    setState(() {});
+                  }
+
+                  // try {
+                  //   final GoogleAPIClient httpClient = GoogleAPIClient(await _currentUser!.authHeaders);
+                  //   final GoogleAPI.CalendarApi calendarApi = GoogleAPI.CalendarApi(httpClient);
+                  //   // Delete the event.
+                  //   await calendarApi.events.delete('primary', details..resource.id);
+                  //   print('Event deleted successfully');
+                  // } catch (e) {
+                  //   print('Error deleting event: $e');
+                  // } finally {
+                  //   // Close the client to release resources.
+                  //   client.close();
+                  // }
+
+
+                  if (details.targetElement == CalendarElement.appointment) {
+                    // An event is tapped
+                    print('Event tapped: ${details.appointments![0].subject}');
+                    // You can perform actions here when an event is tapped
+                  }
+                },
+                controller: _controller,
+                onViewChanged: (onViewChanged){
+
+                },
+                view: CalendarView.schedule,
+                initialDisplayDate: DateTime.now(),
+                //(2024, 1, 15, 9, 0, 0),
+                dataSource: GoogleDataSource(events: snapshot.data),
+                monthViewSettings: const MonthViewSettings(
+                    appointmentDisplayMode:
+                        MonthAppointmentDisplayMode.appointment),
+                allowAppointmentResize: true,
+                showDatePickerButton: false,
+                showWeekNumber: true,
+                showNavigationArrow: true,
+                showCurrentTimeIndicator: true,
+                allowViewNavigation: false,
+                allowDragAndDrop: true,
+              ),
+              snapshot.data != null
+                  ? Container()
+                  : const Center(
+                      child: CircularProgressIndicator(),
+                    )
+            ],
+          );
+        },
+      ),
+      floatingActionButton: FloatingActionButton.extended(onPressed: () async{
+
+        final GoogleAPIClient httpClient = GoogleAPIClient(await _currentUser!.authHeaders);
+        final GoogleAPI.CalendarApi calendarApi = GoogleAPI.CalendarApi(httpClient);
+        var event = GoogleAPI.Event()
+          ..summary = 'Event Summary New '
+          ..description = 'Event Description New'
+          ..start = GoogleAPI.EventDateTime(dateTime: DateTime.now().subtract(const Duration(days: 1)), timeZone: 'GMT')
+          // ..dateTime = DateTime.now().add(Duration(days: 1))
+          // ..timeZone = 'GMT'
+          ..end = GoogleAPI.EventDateTime(dateTime: DateTime.now().add(const Duration(days: 1, hours: 1)), timeZone: 'GMT')
+          // ..dateTime = DateTime.now().add(Duration(days: 1, hours: 1))
+          // ..timeZone = 'GMT'
+        ;
+
+        var calendarId = 'primary'; // Use 'primary' for the user's primary calendar
+        final status = await calendarApi.events.insert(event, calendarId);
+        setState(() {});
+        print('Add Event Status --> ${status.description}');
+
+      }, label: const Text('Add Event', style: TextStyle(fontWeight: FontWeight.w700, fontSize: 20))),
+    );
+  }
+
+  @override
+  void dispose() {
+    if (_googleSignIn.currentUser != null) {
+      _googleSignIn.disconnect();
+      _googleSignIn.signOut();
+    }
+
+    super.dispose();
+  }
+}
+
+class GoogleDataSource extends CalendarDataSource {
+  GoogleDataSource({required List<GoogleAPI.Event>? events}) {
+    appointments = events;
+  }
+
+  @override
+  DateTime getStartTime(int index) {
+    final GoogleAPI.Event event = appointments![index];
+    return event.start?.date ?? event.start!.dateTime!.toLocal();
+  }
+
+  @override
+  bool isAllDay(int index) {
+    return appointments![index].start.date != null;
+  }
+
+  @override
+  DateTime getEndTime(int index) {
+    final GoogleAPI.Event event = appointments![index];
+    return event.endTimeUnspecified != null && event.endTimeUnspecified!
+        ? (event.start?.date ?? event.start!.dateTime!.toLocal())
+        : (event.end?.date != null
+            ? event.end!.date!.add(const Duration(days: -1))
+            : event.end!.dateTime!.toLocal());
+  }
+
+  @override
+  String getLocation(int index) {
+    return appointments![index].location ?? '';
+  }
+
+  @override
+  String getNotes(int index) {
+    return appointments![index].description ?? '';
+  }
+
+  @override
+  String getSubject(int index) {
+    final GoogleAPI.Event event = appointments![index];
+    return event.summary == null || event.summary!.isEmpty
+        ? 'No Title'
+        : event.summary!;
+  }
+}
+
+class GoogleAPIClient extends IOClient {
+  final Map<String, String> _headers;
+
+  GoogleAPIClient(this._headers) : super();
+
+  @override
+  Future<IOStreamedResponse> send(BaseRequest request) =>
+      super.send(request..headers.addAll(_headers));
+
+  @override
+  Future<Response> head(Uri url, {Map<String, String>? headers}) =>
+      super.head(url,
+          headers: (headers != null ? (headers..addAll(_headers)) : headers));
+}
+
 class MapScreen extends StatefulWidget {
   const MapScreen({super.key});
 
@@ -401,7 +640,8 @@ class MapScreenState extends State<MapScreen> {
 
     for (var i = 0; i < data!.length; i++) {
       try {
-        print('Loop Calling....Len ${data.length} --- ${data.length-1 == i}... ${data[i].name}  ${data[i].location?.lng}');
+        print(
+            'Loop Calling....Len ${data.length} --- ${data.length - 1 == i}... ${data[i].name}  ${data[i].location?.lng}');
         final user = data[i];
         PolylineId id = const PolylineId("poly");
         // polylineCoordinates.add(LatLng(data![i].location!.lat,
@@ -411,7 +651,7 @@ class MapScreenState extends State<MapScreen> {
         PolylineResult data1 = await polylinePoints.getRouteBetweenCoordinates(
             "AIzaSyBwgA1lUXJCuACf_9-Zy-fuYULV1W8HEqM",
             PointLatLng(data[i].location!.lat, data[i].location!.lng),
-            (data.length-1) == i
+            (data.length - 1) == i
                 ? PointLatLng(data[i].location!.lat, data[i].location!.lng)
                 : PointLatLng(
                     data[i + 1].location!.lat, data[i + 1].location!.lng),
@@ -442,7 +682,6 @@ class MapScreenState extends State<MapScreen> {
 
         //Time Calculate
 
-
         // Location loc1 = Location(lat: 0 == i ? data[i].location!.lat : data[i - 1].location!.lat, lng: 0 == i ? data[i].location!.lng : data[i - 1].location!.lng);
         // Location loc2 = Location(lat: data[i].location!.lat,lng: data[i].location!.lng,);
         // double distance = loc1.distanceTo(loc2);
@@ -454,9 +693,10 @@ class MapScreenState extends State<MapScreen> {
           Marker(
             draggable: true,
             flat: false,
-            infoWindow: InfoWindow(title: data[i].name, snippet: totalDistance.toStringAsFixed(2)),
+            infoWindow: InfoWindow(
+                title: data[i].name, snippet: totalDistance.toStringAsFixed(2)),
             markerId: MarkerId('${user.name} position $i'),
-            icon: user.name == '${data![0].name}'
+            icon: user.name == '${data[0].name}'
                 ? BitmapDescriptor.defaultMarkerWithHue(
                     BitmapDescriptor.hueRed,
                   )
